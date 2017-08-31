868958bac41b9b54a899f0f82f4e177bf046a75c
Swetha Mandava <sweth.mandava@gmail.com>
2017-08-30 23:30:33 -0400

    Question 2 corrections

--- 2_code/2_test.m
+++ 2_code/2_test.m
@@ -1,16 +1,32 @@
-load('TRAIN_knn.mat')
 X_train = [];
-for i = 1:size(X,1)
-    X_train = [X_train; X{i}];
+train_files = dir('2\train_aug\*.png');
+num_files = size(train_files,1);
+for i = 1:num_files
+    filename = train_files(i).name
+    label = strsplit(filename, '.');
+    label = strsplit(label{1}, '_');
+    label = str2num(label{1});
+    
+    I = imread(['2\train_aug\', filename]); 
+%     Select 60 strongest SURF features of the given image
+    points = detectSURFFeatures(I);
+    points = points.selectStrongest(60);
+    [features, points] = extractFeatures(I, points);
+    features = reshape(features, [1,60,64]);
+    X_train = [X_train; features];
 end
+
 Y = 1:1000;
 Y = repmat(Y, 960,1);
 Y = Y(:);
 
+% Fit a KNN to the data
 mdl = fitcknn(X_train, Y, 'NumNeighbors', 1)
 
 Y_test = zeros(10000,1);
 test_files = dir('2/test/');
+
+% Use the model to predict class
 for i = 1:test_files
     file = test_files{i}
     I = imread(['2/test/', file]);
--- 2_code/augment.m
+++ 2_code/augment.m
@@ -1,23 +1,28 @@
+% Augment the clean train images as described in the readme
 files = dir('2\train\*.png');
 for i = 1:size(files,1)
     filename = files(i).name
     I = imread(['2\train\', filename]);
     file = strsplit(filename, '.');
     file = file{1};
-    if(str2num(file) < 547)
-        continue
-    end
+    
+    %Add gaussian noise
     im = imnoise(I, 'Gaussian', 0, 0.2);
     imwrite(im, ['2\train_aug\', file, '_.png']);
     
+    %Rotate image
     for j = 1:7
         ang = j*45;
         i = imrotateq(im, ang);
         f = strcat('2\train_aug\', file, '_', num2str(ang),'.png');
         imwrite(i, f);
     end
+    
+    %Flip image to obtain opposite foot
     im = flip(im, 2);
     imwrite(im, ['2\train_aug\', file, '_f.png']);
+    
+    %Rotate flipped image
     for j = 1:7
         ang = j*45;
         i = imrotateq(im, ang);
